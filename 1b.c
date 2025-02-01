42f3c419c07f695e63393c95db6d47541bea2005
Shiva Charan <75972507+ElixirDrop0001@users.noreply.github.com>
2025-02-01 15:37:24 +0530

    Add files via upload

---
+++ 1b.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char b[100];
+    int i = 0;
+
+
+    printf("\nEnter the string:\n");
+    fgets(b, sizeof(b), stdin);
+
+
+    int len = strlen(b);
+    if (b[len - 1] == '\n') {
+        b[len - 1] = '\0';
+    }
+
+
+    printf("\nAfter stuffing:\n");
+    printf("DLESTX");
+
+
+    while (i < strlen(b)) {
+
+        if ((b[i] == 'd' || b[i] == 'D') && (b[i+1] == 'l' || b[i+1] == 'L') && (b[i+2] == 'e' || b[i+2] == 'E')) {
+
+            printf("DLE");
+            i += 3;
+        }
+        else
+        {
+            printf("%c", b[i]);
+            i++;
+        }
+    }
+    printf("DLEETX\n");
+}
